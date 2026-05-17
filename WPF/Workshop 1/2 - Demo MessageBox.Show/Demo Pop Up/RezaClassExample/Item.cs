9a35cec4aee1c07bacb117d70a1ba1614bf11e9d
Reza Mirjahanian <rmirjahanian@gmail.com>
2026-05-17 09:51:21 +0330

    Demo MessageBox.Show

---
+++ WPF/Workshop 1/2 - Demo MessageBox.Show/Demo Pop Up/RezaClassExample/Item.cs
@@ -0,0 +1,18 @@
+﻿namespace RezaClassExample
+{
+    class Item
+    {
+        public string FirstData { get; set; }
+        public string LastData { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstData} {LastData}";
+        }
+
+        public void Run()
+        {
+            System.Windows.MessageBox.Show("First WPF");
+        }
+    }
+}
---
+++ WPF/Workshop 1/2 - Demo MessageBox.Show/Demo Pop Up/RezaClassExample/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace RezaClassExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Clicked(object sender, RoutedEventArgs e)
+        {
+            Item em = new Item();
+            em.Run();
+        }
+    }
+}
