a66678ad3b6c6cef7967e39df229ca26daef41e3
Reza Mirjahanian <rmirjahanian@gmail.com>
2026-05-18 20:06:05 +0330

    WPF Panels

---
+++ WPF/Workshop 1/20 - Demo ToggleButton/Source/ToggleButton Reza/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToggleButton_Reza
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void tglButton_Checked(object sender, RoutedEventArgs e)
+        {
+            Label.Content = " Output is enabled...";
+            Background = Brushes.Chartreuse;
+        }
+
+        private void tglButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Label.Content=" Output is disabled...";
+            Background = Brushes.LightGray;
+        }
+    }
+}
