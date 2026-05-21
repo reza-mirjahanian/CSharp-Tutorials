a5a54246e014be6ec8205628dacfebfcfb81af48
Reza Mirjahanian <rmirjahanian@gmail.com>
2026-05-21 10:42:24 +0330

    WPF demo

---
+++ WPF/Workshop 1/22 - Demo CheckBox/Source/CheckBox Reza/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CheckBox_Reza
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void choc_Checked(object sender, RoutedEventArgs e)
+        {
+            Label1.Content = "Extra chocolate";
+        }
+
+        private void choc_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Label1.Content = " ";
+        }
+
+        private void sugar_Checked(object sender, RoutedEventArgs e)
+        {
+            Label2.Content = "No sugar";
+        }
+
+        private void sugar_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Label2.Content = " ";
+        }
+    }
+}
